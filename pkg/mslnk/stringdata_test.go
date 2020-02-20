36d67f6159d5961a2e3a9d59046d418c4c9967af
Wiktor <wykwit@disroot.org>
2020-02-20 20:48:13 +0100

    Move StringData tests to a test package

--- pkg/mslnk/stringdata_test.go
+++ pkg/mslnk/stringdata_test.go
@@ -1,15 +1,16 @@
-package mslnk
+package mslnk_test
 
 import (
 	"bytes"
+	"github.com/gotopkg/mslnk/pkg/mslnk"
 	"testing"
 )
 
 func TestStringdata(t *testing.T) {
 	println("Testing StringData...")
-	x := StringData{
-		"RelativePath":         StringDataStruct("testpath"),
-		"CommandLineArguments": StringDataStruct("testABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABC"),
+	x := mslnk.StringData{
+		"RelativePath":         mslnk.StringDataStruct("testpath"),
+		"CommandLineArguments": mslnk.StringDataStruct("testABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABC"),
 	}
 	expected := []byte{8, 0, 116, 101, 115, 116, 112, 97, 116, 104, 3, 1, 116, 101, 115, 116, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67, 65, 66, 67}
 	if !bytes.Equal(x.Bytes(), expected) {
@@ -19,11 +20,11 @@ func TestStringdata(t *testing.T) {
 
 func TestStringdata_Update(t *testing.T) {
 	println("Testing StringData header update...")
-	h := Header()
-	x := StringData{
-		"NameString":           StringDataStruct("test name"),
-		"RelativePath":         StringDataStruct("test path"),
-		"CommandLineArguments": StringDataStruct("test argument"),
+	h := mslnk.Header()
+	x := mslnk.StringData{
+		"NameString":           mslnk.StringDataStruct("test name"),
+		"RelativePath":         mslnk.StringDataStruct("test path"),
+		"CommandLineArguments": mslnk.StringDataStruct("test argument"),
 	}
 	x.Update(&h)
 	if !(h.LinkFlags["HasName"] && h.LinkFlags["HasArguments"] && h.LinkFlags["HasRelativePath"]) {
